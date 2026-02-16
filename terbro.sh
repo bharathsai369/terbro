4be026b2ec06eb5238ea863e592ac07591aea09d
Bharath Sai <bharathsai257@gmail.com>
2026-02-16 19:17:07 +0530

    fixed some bugs

--- terbro.sh
+++ terbro.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-APP="$HOME/Documents/workspace/terbro"
+APP="$HOME/Documents/workspace/ter-bro-reads"
 VENV="$APP/tbr-env"
 PY="$VENV/bin/python"
 SCRIPT="$APP/main.py"
 
-# If user asks for help, history, or saving a file, don't use the pager
-if [[ "$*" == *"--help"* ]] || [[ "$*" == *"-h"* ]] || [[ "$*" == *"--save"* ]] || [[ "$*" == *"--history"* ]] || [[ "$*" == *"--search"* ]]; then
-    "$PY" "$SCRIPT" "$@"
+# Capture all flags
+ARGS="$@"
+
+# Check if we should SKIP the pager (less)
+# We skip if no args, or if help/save/history is involved
+if [[ -z "$ARGS" ]] || [[ "$ARGS" == *"--help"* ]] || [[ "$ARGS" == *"-h"* ]] || [[ "$ARGS" == *"--save"* ]] || [[ "$ARGS" == *"--history"* ]] || [[ "$ARGS" == *"--search"* ]]; then
+    "$PY" "$SCRIPT" $ARGS
 else
-    # Use -R for ANSI color support in less
-    "$PY" "$SCRIPT" "$@" | less -R -M -i -j5
+    # -R : Allows ANSI colors in less
+    # -i : Case-insensitive search inside less
+    # -+G: FORCE less to start at the top (Line 1)
+    "$PY" "$SCRIPT" $ARGS | less -R -i -M -+G
 fi
